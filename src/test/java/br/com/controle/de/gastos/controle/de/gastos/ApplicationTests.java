6d35dac0428bcdc5dced97b5084aff03f70e85fc
danieligor56 <52861750+danieligor56@users.noreply.github.com>
2023-10-21 16:25:36 -0300

    Versão inicial do projeto

---
+++ src/test/java/br/com/controle/de/gastos/controle/de/gastos/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.controle.de.gastos.controle.de.gastos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
